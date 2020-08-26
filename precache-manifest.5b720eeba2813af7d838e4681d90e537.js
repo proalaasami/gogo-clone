d83d38dc5ba13936eb4bc98b8e1b3ee1b01b68dd
gamal646 <gamal646@gamal646s-MacBook-Air.local>
2020-08-26 08:03:17 +0200

    Updates

---
+++ precache-manifest.5b720eeba2813af7d838e4681d90e537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff641a56a2089cd3c22721aafe64c219",
+    "url": "/gogo-clone/index.html"
+  },
+  {
+    "revision": "e9420f5e087ea328bab0",
+    "url": "/gogo-clone/static/css/main.a59f75eb.chunk.css"
+  },
+  {
+    "revision": "d4f3a9af3e826e5cb100",
+    "url": "/gogo-clone/static/js/2.37965cf0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gogo-clone/static/js/2.37965cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9420f5e087ea328bab0",
+    "url": "/gogo-clone/static/js/main.b14af3e1.chunk.js"
+  },
+  {
+    "revision": "463cddfdeced81a363b6",
+    "url": "/gogo-clone/static/js/runtime-main.721bfb40.js"
+  }
+]);
